9a9df448cc300da0a181ce0d9ee583b23529f775
Artem Klevakin <a.klevakin@elama.ru>
2019-04-21 21:41:49 +0300

    add google tags

--- landing/components/Head/index.jsx
+++ landing/components/Head/index.jsx
@@ -1,9 +1,31 @@
 import Head from 'next/head'
 import '../../styles/global.scss'
 
+const googleTagManger = `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-K4FPF86');
+`
+
+const googleAnalytics = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-138742912-1');
+`
+
 export default ({ title }) => (
   <Head>
     <meta charSet="utf-8"/>
+    {process.env.NODE_EVN === 'production' && (
+      <>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138742912-1"></script>
+        <script dangerouslySetInnerHTML={{__html: googleTagManager}} />
+        <script dangerouslySetInnerHTML={{__html: googleAnalytics}} />
+      </>
+    )}
     <title>
         {title || 'SkillHunter | Платформа для развития профессиональных и личных навыков'}
     </title>
