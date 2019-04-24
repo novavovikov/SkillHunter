5ff7b2bc28cb505f9519d95192d14019b29681a2
Vladimir Novikov <novavovikov@gmail.com>
2019-04-24 23:46:54 +0300

    clear container

---
+++ build/clear.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if ! docker ps -q &> /dev/null
+then
+    echo "You must be in docker group or root"
+    exit 1
+fi
