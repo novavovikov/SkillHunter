71519bd4c2d3e17b11e111972a7eb890fb72492f
Vladimir Novikov <novavovikov@gmail.com>
2019-04-25 20:47:23 +0300

    clear

---
+++ build/clear.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if ! docker ps -q &> /dev/null
+then
+    echo "You must be in docker group or root"
+    exit 1
+fi
+
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -a -q)
+docker volume prune
