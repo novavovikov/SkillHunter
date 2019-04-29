d403ebf7739f06202b8a2d8ead95871958624c97
zhilinstas <zhilin.stas@gmail.com>
2019-04-30 00:42:25 +0300

    добавил tslint и prettier

--- frontend/packages/base/src/App.tsx
+++ frontend/packages/base/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { FunctionComponent } from 'react'
 import { Text, View } from 'react-native'
 // @ts-ignore
-import { styles as s} from 'react-native-style-tachyons'
+import { styles as s } from 'react-native-style-tachyons'
 
 const App: FunctionComponent = () => {
   return (
     <View style={[s.ba, s.jcfs, s.pa2]}>
-      <Text>
-        Skillhunter
-      </Text>
+      <Text>Skillhunter</Text>
     </View>
   )
 }
--- frontend/packages/web/config-overrides.js
+++ frontend/packages/web/config-overrides.js
@@ -5,10 +5,7 @@ const webpack = require('webpack')
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
-const appIncludes = [
-  resolveApp('src'),
-  resolveApp('../base/src'),
-]
+const appIncludes = [resolveApp('src'), resolveApp('../base/src')]
 
 module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(
@@ -21,9 +18,7 @@ module.exports = function override(config, env) {
     require.resolve('babel-plugin-react-native-web')
   ].concat(config.module.rules[2].oneOf[1].options.plugins)
   config.module.rules = config.module.rules.filter(Boolean)
-  config.plugins.push(
-    new webpack.DefinePlugin({ __DEV__: env !== 'production' })
-  )
+  config.plugins.push(new webpack.DefinePlugin({ __DEV__: env !== 'production' }))
 
   return config
 }
