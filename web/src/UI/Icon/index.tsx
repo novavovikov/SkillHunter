dd22d109f1734cb7a814a07d944e110d7f1ac330
Vladimir Novikov <novavovikov@gmail.com>
2019-07-11 10:34:06 +0300

    icons

--- web/src/UI/Icon/index.tsx
+++ web/src/UI/Icon/index.tsx
@@ -5,19 +5,20 @@ import arrowDownIcon from './icons/arrow-down-ico.svg?inline'
 import arrowRightIcon from './icons/arrow-right-ico.svg?inline'
 import arrowUpIcon from './icons/arrow-up-ico.svg?inline'
 import binIcon from './icons/bin-ico.svg?inline'
+import closeIcon from './icons/close-ico.svg?inline'
+import copyIcon from './icons/copy-ico.svg?inline'
 import dotsIcon from './icons/dots-ico.svg?inline'
+import facebookIcon from './icons/facebook-ico.svg?inline'
 import heartFilledIcon from './icons/heart-filled-ico.svg?inline'
 import heartIcon from './icons/heart-ico.svg?inline'
-import shareIcon from './icons/share-ico.svg?inline'
-import userIcon from './icons/user-ico.svg?inline'
-import closeIcon from './icons/close-ico.svg?inline'
-import removeIcon from './icons/remove-ico.svg?inline'
-import copyIcon from './icons/copy-ico.svg?inline'
-import facebookIcon from './icons/facebook-ico.svg?inline'
 import linkedinIcon from './icons/linkedin-ico.svg?inline'
 import redditIcon from './icons/reddit-ico.svg?inline'
+import addIcon from './icons/add-ico.svg?inline'
+import removeIcon from './icons/remove-ico.svg?inline'
+import shareIcon from './icons/share-ico.svg?inline'
 import telegramIcon from './icons/telegram-ico.svg?inline'
 import twitterIcon from './icons/twitter-ico.svg?inline'
+import userIcon from './icons/user-ico.svg?inline'
 import vkIcon from './icons/vk-ico.svg?inline'
 
 interface Props {
@@ -41,18 +42,23 @@ const getIcon = (type: string) => {
       return shareIcon
     case 'dots':
       return dotsIcon
+
     case 'arrow-up':
       return arrowUpIcon
     case 'arrow-down':
       return arrowDownIcon
     case 'arrow-right':
       return arrowRightIcon
+
     case 'close':
       return closeIcon
+    case 'add':
+      return addIcon
     case 'remove':
       return removeIcon
     case 'copy':
       return copyIcon
+
     case 'facebook':
       return facebookIcon
     case 'twitter':
--- web/src/components/ResourceHeader/index.tsx
+++ web/src/components/ResourceHeader/index.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import React, { PureComponent } from 'react'
+import { Icon } from '../../UI'
 import * as s from './ResourceHeader.css'
 
 interface State {
@@ -31,18 +32,48 @@ class ResourceHeader extends PureComponent<{}, State> {
         [s.ResourceHeader_fixed]: fixedClass,
       })}>
         <div className={s.ResourceHeader__item}>
+          <div className={s.ResourceHeader__icon}>
+            <Icon
+              type="dots"
+              size="18"
+            />
+          </div>
           Backlog
         </div>
         <div className={s.ResourceHeader__item}>
+          <div className={s.ResourceHeader__icon}>
+            <Icon
+              type="heart"
+              size="18"
+            />
+          </div>
           1000000
         </div>
         <div className={s.ResourceHeader__item}>
+          <div className={s.ResourceHeader__icon}>
+            <Icon
+              type="share"
+              size="18"
+            />
+          </div>
           Share
         </div>
         <div className={s.ResourceHeader__item}>
+          <div className={s.ResourceHeader__icon}>
+            <Icon
+              type="add"
+              size="18"
+            />
+          </div>
           2 skills
         </div>
         <div className={s.ResourceHeader__item}>
+          <div className={s.ResourceHeader__icon}>
+            <Icon
+              type="dots"
+              size="18"
+            />
+          </div>
           More
         </div>
       </header>
