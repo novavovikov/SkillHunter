98ff47e38163f34ad51aa3c5c47861daf3edcad3
Vladimir Novikov <novavovikov@gmail.com>
2019-08-21 03:09:51 +0300

    switch

--- web/src/components/FatalError/index.tsx
+++ web/src/components/FatalError/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 const FatalError: FC = () => {
   return (
     <div className={'fatal-error'}>
-      Что-то пошло не так. Проверьте подключение к серверу
+      Something went wrong. Check server connection
     </div>
   )
 }
