7353a8ba4349b24d637b55f4e43d0e66b39d95d0
Vladimir Novikov <novavovikov@gmail.com>
2019-08-30 22:25:28 +0300

    add sw

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import App from './app'
+import * as serviceWorker from './serviceWorker'
 import './styles/global.css'
 
 const rootEl = document.getElementById('app')
@@ -24,3 +25,8 @@ if (module.hot) {
     )
   })
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
