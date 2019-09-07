9e00cf475b91147af5776676478b18e6e88375f3
Vladimir Novikov <novavovikov@gmail.com>
2019-09-07 14:41:16 +0300

    change to prof

--- backend/src/constants/welcome.ts
+++ backend/src/constants/welcome.ts
@@ -1,2 +1,2 @@
 export const WELCOME_SKILL_NAME = 'Skillhunter'
-export const WELCOME_RESOURCE_ID = process.env.NODE_ENV === 'production' ? 952 : 1
+export const WELCOME_RESOURCE_ID = process.env.NODE_ENV === 'prod' ? 952 : 1
--- backend/src/modules/telegram/constants/telegram.ts
+++ backend/src/modules/telegram/constants/telegram.ts
@@ -1,8 +1,8 @@
 export const TELEGRAM_BOT_ID = process.env.TELEGRAM_BOT_ID
 
 export const TELEGRAM_URI_BY_ENV = {
-  production: `https://api.telegram.org/bot`,
-  development: `https://app.skillhunter.io/api/telegram/`,
+  prod: `https://api.telegram.org/bot`,
+  dev: `https://app.skillhunter.io/api/telegram/`,
 }
 
 export const TELEGRAM_URI = `${TELEGRAM_URI_BY_ENV[process.env.NODE_ENV]}${TELEGRAM_BOT_ID}`
