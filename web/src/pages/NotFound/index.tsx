a27dac9157cbde60a3995e1cec3cbd05682c0173
Vladimir Novikov <novavovikov@gmail.com>
2019-09-23 23:50:15 +0300

    proxy

--- web/src/pages/NotFound/index.tsx
+++ web/src/pages/NotFound/index.tsx
@@ -1,54 +1,67 @@
-import React, { Component, FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Typed from 'typed.js'
+import notFoundImg from './images/not-found.svg'
 import { ROUTES } from '../../constants/routing'
 import { analytics } from '../../utils/analytics'
-import notFoundImg from './images/not-found.svg'
 import * as s from './NotFound.css'
 
-class NotFound extends Component {
-  componentDidMount (): void {
+const NotFound: FC = () => {
+  useEffect(() => {
     analytics({
       event: '404',
-      category: 'error'
+      category: 'error',
     })
-  }
 
-  render () {
-    return (
-      <div className={s.NotFound}>
-        <div className={s.NotFound__img}>
-          <img src={notFoundImg} alt=""/>
-        </div>
-        <div className={s.NotFound__title}>
-          Page not found
-        </div>
-        <div className={s.NotFound__subtitle}>
-          404
-        </div>
+    const
+      options = {
+        strings: [
+          `Page not found`,
+        ],
+        typeSpeed: 50,
+        backSpeed: 40,
+      }
 
-        <Link
-          to={ROUTES.INTRODUCTION}
-          className={s.NotFound__submit}
-        >
-          Create
-          <span className={s.NotFound__submitLabel}>
-          skillset
-        </span>
-        </Link>
+    const
+      typedText = new Typed('[data-typed]', options)
 
-        <div className={s.NotFound__label}>
-          or
-        </div>
+    return () => typedText.destroy()
+  })
 
-        <Link
-          to={ROUTES.HOME}
-          className={s.NotFound__link}
-        >
-          Go to home page
-        </Link>
+  return (
+    <div className={s.NotFound}>
+      <div className={s.NotFound__img}>
+        <img src={notFoundImg} alt=""/>
       </div>
-    )
-  }
+      <div className={s.NotFound__title}>
+        <span data-typed=""/>
+      </div>
+      <div className={s.NotFound__subtitle}>
+        404
+      </div>
+
+      <Link
+        to={ROUTES.INTRODUCTION}
+        className={s.NotFound__submit}
+      >
+        Create
+        <span className={s.NotFound__submitLabel}>
+        skillset
+      </span>
+      </Link>
+
+      <div className={s.NotFound__label}>
+        or
+      </div>
+
+      <Link
+        to={ROUTES.HOME}
+        className={s.NotFound__link}
+      >
+        Go to home page
+      </Link>
+    </div>
+  )
 }
 
 export default NotFound
